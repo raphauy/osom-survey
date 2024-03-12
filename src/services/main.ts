c94e287c0f98797b3a344947f79f3902dbbe06e3
raphauy <rapha.uy@rapha.uy>
2024-03-12 20:28:15 -0300

    categorize responses v2

--- src/services/main.ts
+++ src/services/main.ts
@@ -2,30 +2,10 @@ import { categorizeResponse } from "./category-services"
 
 async function main() {
 
-    const topicResponseId = 'cls9813xf005m7vvxexbu49zs'
+    const topicResponseId = 'clsc3xqqo011c12i0tb7goxm4'
 
-    //await categorizeResponse(topicResponseId)
+    await categorizeResponse(topicResponseId)
 
-    const apiToken= process.env.API_TOKEN
-    if (!apiToken) {
-        console.error("API_TOKEN is not defined")
-        throw new Error("API_TOKEN is not defined")
-    }
-    const baseUrl= process.env.NEXTAUTH_URL
-    const url= `${baseUrl}/api/categorization`
-    console.log("url: ", url)
-    
-    // apiToken is Bearer token
-    const res= await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + apiToken
-        },
-        body: JSON.stringify({"init": true})
-    })
-
-    console.log("res: ", res)
 
 
 }
